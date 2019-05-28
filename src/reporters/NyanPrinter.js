88eda7366597e0594efb9e6905b82544473e95a6
losandes <andes.collab@gmail.com>
2019-05-28 09:51:40 -0400

    Moves Nyan Printer Into Own Function

--- src/reporters/NyanPrinter.js
+++ src/reporters/NyanPrinter.js
@@ -6,118 +6,22 @@ module.exports = Printer
 function Printer (streamPrinter, styles) {
   'use strict'
 
-  const { rainbowify } = new Rainbowifier()
-  const size = streamPrinter.getWindowSize()
-  const width = size.width * 0.75 | 0
-  // const maxHeight = size.height - 1
-  const numberOfLines = 4 // Math.max(4, Math.min(config.numberOfLines, maxHeight))
-  const UP = styles.up(numberOfLines)
-  const nyanCatWidth = 11
-  const scoreboardWidth = 5
-  const trajectoryWidthMax = (width - nyanCatWidth)
   const stats = { total: 0, success: 0, failed: 0, skipped: 0, errors: [] }
-  const trajectories = []
-
-  for (let i = 0; i < numberOfLines; i++) {
-    trajectories[i] = []
-  }
-
-  let tick = 0
-  let specCount = 0
   const printerOutput = []
 
   const print = (line) => {
     streamPrinter.print(line)
   }
 
-  const drawScoreboard = () => {
-    print(` ${styles.cyan(stats.total)}${styles.newLine()}`)
-    print(` ${styles.green(stats.success)}${styles.newLine()}`)
-    print(` ${styles.red(stats.failed)}${styles.newLine()}`)
-    print(` ${styles.yellow(stats.skipped)}${styles.newLine()}`)
-  }
-
-  const drawRainbow = () => {
-    trajectories.forEach((line) => {
-      print(`\u001b[${scoreboardWidth}C${line.join('')}${styles.newLine()}`)
-    })
-  }
-
-  const appendRainbow = () => {
-    const segment = rainbowify(tick ? '_' : '-')
-
-    for (let i = 0; i < numberOfLines; i++) {
-      const trajectory = trajectories[i]
-
-      if (trajectory.length >= trajectoryWidthMax) {
-        trajectory.shift()
-      }
-      trajectory.push(segment)
-    }
-  }
-
-  const face = () => {
-    if (stats.failed) {
-      return '( x .x)'
-    } else if (stats.skipped) {
-      return '( o .o)'
-    } else if (stats.success) {
-      return '( ^ .^)'
-    } else {
-      return '( - .-)'
-    }
-  }
-
-  const drawNyanCat = () => {
-    const startWidth = scoreboardWidth + trajectories[0].length
-    const color = `\u001b[${startWidth}C`
-
-    print(`${color}_,------,${styles.newLine()}`)
-
-    ;(() => {
-      const padding = tick ? '  ' : '   '
-      print(`${color}_|${padding}/\\_/\\ ${styles.newLine()}`)
-    })()
-
-    ;(() => {
-      const padding = tick ? '_' : '__'
-      const tail = tick ? '~' : '^'
-      print(`${color}${tail}|${padding}${face()} ${styles.newLine()}`)
-    })()
-
-    ;(() => {
-      const padding = tick ? ' ' : '  '
-      print(`${color}${padding}  ""  "" ${styles.newLine()}`)
-    })()
-  }
-
-  const fillWithNewLines = (startFrom) => {
-    for (let i = startFrom || 0; i < numberOfLines + 1; i++) {
-      print(styles.newLine())
-    }
-  }
-
-  const draw = () => {
-    fillWithNewLines(numberOfLines + 1)
-    print(UP)
-    appendRainbow()
-    drawScoreboard()
-    fillWithNewLines(numberOfLines + 1)
-    print(UP)
-    drawRainbow()
-    print(UP)
-    drawNyanCat()
-    tick = !tick
-  }
+  const { draw } = new NyanPrinter({
+    print,
+    styles,
+    windowSize: streamPrinter.getWindowSize()
+  })
 
   print.start = function (message) {
-    specCount += 1
-
-    if (specCount === 1) {
-      print(styles.newLine())
-      print(styles.hide())
-      drawScoreboard()
-    }
+    draw.begin(stats)
+    printerOutput.push({ message })
   }
 
   print.startTest = function (message) {
@@ -127,21 +31,21 @@ function Printer (streamPrinter, styles) {
   print.success = function (behavior) {
     stats.success += 1
     stats.total += 1
-    draw()
+    draw(stats)
     printerOutput.push({ behavior, status: 'SUCCESS' })
   }
 
   print.skipped = function (behavior) {
     stats.skipped += 1
     stats.total += 1
-    draw()
+    draw(stats)
     printerOutput.push({ behavior, status: 'SKIPPED' })
   }
 
   print.failed = function (behavior, e) {
     stats.failed += 1
     stats.total += 1
-    draw()
+    draw(stats)
     let err = `${styles.newLine()}${styles.bgRed(styles.black(' ' + behavior))}${styles.newLine()}${styles.newLine()}`
 
     if (e && e.expected && e.actual) {
@@ -171,7 +75,7 @@ function Printer (streamPrinter, styles) {
 
     print(output)
     stats.errors.forEach(print)
-    print(styles.show())
+    draw.end(stats)
   }
 
   print.end = function (message) {
@@ -184,6 +88,117 @@ function Printer (streamPrinter, styles) {
     newLine: styles.newLine(),
     getOutput: () => { return printerOutput.join(styles.newLine()) }
   })
+}
+
+function NyanPrinter (options) {
+  'use strict'
+
+  let initialized = false
+  const { print, styles, windowSize } = options
+  const { rainbowify } = new Rainbowifier()
+  const width = windowSize.width * 0.75 | 0
+  const numberOfLines = 4
+  const UP = styles.up(numberOfLines)
+  const nyanCatWidth = 11
+  const scoreboardWidth = 5
+  const trajectoryWidthMax = (width - nyanCatWidth)
+  const trajectories = [...Array(numberOfLines)].map(() => [])
+  let tick = 0
+
+  const drawScoreboard = (stats) => {
+    print(` ${styles.cyan(stats.total)}${styles.newLine()}`)
+    print(` ${styles.green(stats.success)}${styles.newLine()}`)
+    print(` ${styles.red(stats.failed)}${styles.newLine()}`)
+    print(` ${styles.yellow(stats.skipped)}${styles.newLine()}`)
+  }
+
+  const drawRainbow = () => {
+    trajectories.forEach((line) => {
+      print(`\u001b[${scoreboardWidth}C${line.join('')}${styles.newLine()}`)
+    })
+  }
+
+  const appendRainbow = () => {
+    const segment = rainbowify(tick ? '_' : '-')
+
+    for (let i = 0; i < numberOfLines; i++) {
+      const trajectory = trajectories[i]
+
+      if (trajectory.length >= trajectoryWidthMax) {
+        trajectory.shift()
+      }
+      trajectory.push(segment)
+    }
+  }
+
+  const face = (stats) => {
+    if (stats.failed) {
+      return '( x .x)'
+    } else if (stats.skipped) {
+      return '( o .o)'
+    } else if (stats.success) {
+      return '( ^ .^)'
+    } else {
+      return '( - .-)'
+    }
+  }
+
+  const drawNyanCat = (stats) => {
+    const startWidth = scoreboardWidth + trajectories[0].length
+    const color = `\u001b[${startWidth}C`
+
+    print(`${color}_,------,${styles.newLine()}`)
+
+    ;(() => {
+      const padding = tick ? '  ' : '   '
+      print(`${color}_|${padding}/\\_/\\ ${styles.newLine()}`)
+    })()
+
+    ;(() => {
+      const padding = tick ? '_' : '__'
+      const tail = tick ? '~' : '^'
+      print(`${color}${tail}|${padding}${face(stats)} ${styles.newLine()}`)
+    })()
+
+    ;(() => {
+      const padding = tick ? ' ' : '  '
+      print(`${color}${padding}  ""  "" ${styles.newLine()}`)
+    })()
+  }
+
+  const fillWithNewLines = (startFrom) => {
+    for (let i = startFrom || 0; i < numberOfLines + 1; i++) {
+      print(styles.newLine())
+    }
+  }
+
+  const draw = (stats) => {
+    fillWithNewLines(numberOfLines + 1)
+    print(UP)
+    appendRainbow()
+    drawScoreboard(stats)
+    fillWithNewLines(numberOfLines + 1)
+    print(UP)
+    drawRainbow()
+    print(UP)
+    drawNyanCat(stats)
+    tick = !tick
+  }
+
+  draw.begin = (stats) => {
+    if (!initialized) {
+      initialized = true
+      print(styles.newLine())
+      print(styles.hide())
+      drawScoreboard(stats)
+    }
+  }
+
+  draw.end = (stats) => {
+    print(styles.show())
+  }
+
+  return { draw }
 }
 
 /*
