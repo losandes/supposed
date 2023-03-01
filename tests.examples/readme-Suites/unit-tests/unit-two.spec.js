e4251e86137ebbe5476103262b6f75ecc647ec3b
losandes <andes.collab@gmail.com>
2023-02-28 20:54:23 -0500

    fix: fixes the Suite test
    The README Suite test was running all of the test.node tests. This moves
    it into its own folder and adds a small set of tests for it to run
    against.

---
+++ tests.examples/readme-Suites/unit-tests/unit-two.spec.js
@@ -0,0 +1,11 @@
+const test = require('supposed')
+
+test('unit-two, divide by zero equals infinity', async (t) => {
+  const actual = await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(42 / 0)
+    }, 0)
+  })
+
+  t.strictEqual(actual, Infinity)
+})
